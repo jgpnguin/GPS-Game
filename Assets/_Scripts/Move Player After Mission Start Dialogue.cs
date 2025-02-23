27018f51424bd099c3ca2f5d2742be66b6a0f3b3
DeltaChopper <aarngu251@gmail.com>
2025-02-23 00:00:05 -0800

    Made Intro Cutscene

---
+++ Assets/_Scripts/Move Player After Mission Start Dialogue.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovePlayerAfterMissionStartDialogue : MonoBehaviour
+{
+    public int lastIndex;
+    public Entity player;
+    public Dialogue Dialogue;
+    public TextMeshProUGUI text;
+    public float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((text.text == Dialogue.lines[lastIndex]) && (player.transform.position.x <= 10f)) 
+        {
+            player.transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+    }
+}
